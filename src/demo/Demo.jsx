2a9d8e234dc75d071a9034ccf5a88f792185399f
Hyeon Jeon <hj@hcil.snu.ac.kr>
2024-01-31 17:36:09 -0500

    implemented interface for options

--- src/demo/Demo.jsx
+++ src/demo/Demo.jsx
@@ -7,9 +7,12 @@ import { readLocalJsonFromPublic } from '../utils/helpers';
 const Demo = (props) => {
 
 	const [exampleDataId, setExampleDataId] = useState("FashionMNIST-UMAP");
+	const [brushingId, setBrushingId] = useState("dab"); // distortion-aware brushing
 	const [warning, setWarning] = useState("");
 	const width = CONSTANTS.SIZE;
 	const height = CONSTANTS.SIZE;
+	const brushingsId = Object.keys(CONSTANTS.BRUSHINGS);
+	const brushingsName = brushingsId.map((id) => CONSTANTS.BRUSHINGS[id]);
 
 
 	const exampleDatasets = {
@@ -17,18 +20,19 @@ const Demo = (props) => {
 		"FashionMNIST-PCA": "data/fmnist_small_pca_preprocessed.json",
 	}
 
+
+
 	useEffect(() => {
 		(async () => {
 			try {
 				const data = await readLocalJsonFromPublic(exampleDatasets[exampleDataId]);
-				console.log(data);
 			}
 			catch (error) {
 				setWarning("Error while loading data!!")
 			}
 
 		})();
-	}, [exampleDataId]);
+	});
 
 	// Load the data from the "data" folder in public
 
@@ -44,15 +48,30 @@ const Demo = (props) => {
 			<h1 className={styles.mainTitle}>Multidimensional Brushing</h1>
 			<div className={styles.container}>
 				<div className={styles.canvasContainer}>
-					<div>
-						<select className={styles.dropdown} id="exampleDatasets" name="exampleDatasets" onChange={(e) => {
-							setWarning("");
-							setExampleDataId(e.target.value);
-						}}>
-							{Object.keys(exampleDatasets).map((key, index) => {
-								return <option key={index} value={key}>{key}</option>
-							})}
-						</select>
+					<div className={styles.optionsContainer}>
+						<div className={styles.singleOptionContainer}>
+							<h3>{"Dataset: "}</h3>
+							<select className={styles.dropdown} id="exampleDatasets" name="exampleDatasets" onChange={(e) => {
+								setWarning("");
+								setExampleDataId(e.target.value);
+							}}>
+								{Object.keys(exampleDatasets).map((key, index) => {
+									return <option key={index} value={key}>{key}</option>
+								})}
+							</select>
+						</div>
+						<div className={styles.singleOptionContainer}>
+							<h3>{"Brushing technique:"}</h3>
+							<select className={styles.dropdown} id="brushings" name="brushings" onChange={(e) => {
+								setWarning("");
+								setBrushingId(e.target.value);
+							}}>
+								{brushingsId.map((id, index) => {
+									return <option key={index} value={id}>{brushingsName[index]}</option>
+								})}
+							</select>
+						</div>
+
 					</div>
 					<div className={styles.warningContainer}>
 						{warning !== "" && <p className={styles.warning}>{warning}</p>}
