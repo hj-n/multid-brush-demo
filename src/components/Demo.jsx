97401fc53a83979791c3d5708ca37dce282e126a
Hyeon Jeon <hj@hcil.snu.ac.kr>
2024-01-31 16:16:33 -0500

    multidimensional brush demo initiated

---
+++ src/components/Demo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Demo.module.scss';
+
+
+const Demo = (props) => {
+
+	return (
+		<>
+			<h1 className={styles.mainTitle}>Multidimensional Brushing</h1>
+			
+		</>
+	)
+}
+
+export default Demo;
